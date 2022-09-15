96052e0c675942a55f12a308afd6ecdf052f7b03
Anton Pavlov <toni.pavlov.85@bk.ru>
2022-09-15 16:40:16 +0300

    Initial Commit

---
+++ VirtualRealtor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VirtualRealtor
+//
+//  Created by Антон Павлов on 15.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
